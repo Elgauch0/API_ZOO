6631aa5fea157fc4a91b6c8821d7a984518552f5
Anass El kaouri <elkaourianass@gmail.com>
2024-09-24 12:47:07 +0200

    jwt installé

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -155,4 +155,13 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         return $this->userRoles;
     }
+    /**
+     * Méthode getUsername qui permet de retourner le champ qui est utilisé pour l'authentification.
+     *
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->getUserIdentifier();
+    }
 }
